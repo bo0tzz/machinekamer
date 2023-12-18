b96657fbb225b5ded526a01cfc319fa4aee3dc74
bo0tzz <git@bo0tzz.me>
2023-12-18 14:08:11 +0100

    Install deps

--- mix.exs
+++ mix.exs
@@ -22,8 +22,9 @@ defmodule Machinekamer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:emqtt, github: "emqx/emqtt", tag: "1.10.1"},
+      {:jason, "~> 1.4"},
+      {:phoenix_pubsub, "~> 2.1"}
     ]
   end
 end
